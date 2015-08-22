c0756794beaae5d6a4255d092f2def4d0bb95813
Sarah Tsumayoi <tsuharusarah873@gmail.com>
2015-08-22 07:14:27 -0700

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -5,7 +5,7 @@ if (!defined("IN_ESOTALK")) exit;
 
 ET::$pluginInfo["Verified"] = array(
 	"name" => "Verified",
-	"description" => "Verified member.",
+	"description" => "Verify a member.",
 	"version" => "0.0.1",
 	"author" => "Felli",
 	"authorEmail" => "felli-sama@asia.com",
