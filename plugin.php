c28d64ba729ae8d4e3648b2976d5b900d2db60ef
Sarah Tsumayoi <tsuharusarah873@gmail.com>
2015-08-22 07:02:30 -0700

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+// Copyright 2015 Felli
+
+if (!defined("IN_ESOTALK")) exit;
+
+ET::$pluginInfo["Verified"] = array(
+	"name" => "Verified",
+	"description" => "Verified member.",
+	"version" => "0.0.1",
+	"author" => "Felli",
+	"authorEmail" => "felli-sama@asia.com",
+	"authorURL" => "http://www.yuriqueen.ga/blog/",
+	"license" => "GPLv2"
+);
